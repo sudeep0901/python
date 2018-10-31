6e48d81e1da576a0a412786c488077719abcd9d5
sudeep patel <sudeep.tech.patel@gmail.com>
2018-10-31 20:24:38 +0530

    derived classs

--- oppspython/student.py
+++ oppspython/student.py
@@ -1,9 +1,12 @@
-
 students = []
 
+#all methods are public no access modifiers
 
 class Student:
     pass
+
+    # Class Attribute
+    schoolName = "New Delhli Public School"
     # adding methods to classs
     # Constructer Method
 
@@ -22,6 +25,9 @@ class Student:
     def get_name_captalize(self):
         return self.name.capitalize()
 
+    def get_School_name(self):
+        return self.schoolName
+
 
 student = Student("Sudeep Patel", 1)
 
@@ -29,6 +35,28 @@ new_student = Student("Sanjay", 2)
 print(str(student))
 print(new_student)
 print(student.get_name_captalize())
-
+print(student.get_School_name())
+print(Student.schoolName)  # Class Attribute
 # student.add_student('Sudeep', 1)
 # student.add_student('Mark', 2)
+
+
+# Inheritance
+# Student parent class
+# HighSchool - Derived School
+class HighSchool(Student):
+    pass
+    schoolName = "Derived New Delhi School"
+
+    def get_School_name(self):
+        return self.schoolName + "High School"
+
+    # super is parent class
+    def get_name_captalize(self):
+        origional_value = super().get_name_captalize()
+        return origional_value + "Derive class modified return after using parent class method"
+
+
+highSchoolStudent = HighSchool('Manasvi', 10)
+print(highSchoolStudent.get_name_captalize())
+print(highSchoolStudent.get_School_name())
