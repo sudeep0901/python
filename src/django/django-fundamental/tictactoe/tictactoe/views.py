6744cbdff35eb7c679332a9c4eb551f337bc9943
Sudeep Patel <sudeep.tech.patel@gmail.com>
2019-10-21 15:50:54 +0530

    master child layout added{

--- src/django/django-fundamental/tictactoe/tictactoe/views.py
+++ src/django/django-fundamental/tictactoe/tictactoe/views.py
@@ -1,3 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
 def welcome(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request, 'tictactoe/welcome.html')
