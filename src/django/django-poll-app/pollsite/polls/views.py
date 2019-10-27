3fa1549d3dfb5a702927da144702a9949b8cf151
Sudeep Patel <sudeep.tech.patel@gmail.com>
2019-10-27 13:36:47 +0530

    result web page working

--- src/django/django-poll-app/pollsite/polls/views.py
+++ src/django/django-poll-app/pollsite/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 # Create your views here.
-from .models import Question
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
 
 
 def index(request):
@@ -10,6 +11,7 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     print(request)
     print(dir(request))
+
     print(request.is_secure(), request.path_info, request.get_host())
     print(request.body)
     print(request.session)
@@ -38,9 +40,33 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You are asking %s" % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    # response = "You are asking %s" % question_id
+    # return HttpResponse(response)
 
 
+# def vote(request, question_id):
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    print("Post data. . . . .:")
+    print(request.POST)
+    print(request.POST['csrfmiddlewaretoken'])
+    question = get_object_or_404(Question, pk=question_id)
+    print(dir(question))
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        # return HttpResponse("You're voting on question %s." % question_id)
